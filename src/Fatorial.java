e6925de834fa5d712a4cf5c902b2485b4976f407
Caio Ribas <caioribasemm@gmail.com>
2025-10-30 21:57:54 -0300

    Implementcao do fatorial recursivo

---
+++ src/Fatorial.java
@@ -0,0 +1,19 @@
+public class Fatorial {
+
+    public static void main(String[] args) {
+        System.out.println(Fatorial(4));
+    }
+
+    public static int Fatorial(int n){
+
+        if(n==1){
+            return 1;
+        }
+        else {
+            return n*Fatorial(n-1);
+        }
+
+
+    }
+
+}
